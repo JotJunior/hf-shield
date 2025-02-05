eeb40593d361e137235cd51ffbdf6647eebe08ce
Joao G Zanon Jr <Joao Zanon>
2025-02-05 03:12:09 -0300

    Start User and Client controllers

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,27 +3,29 @@
 namespace Jot\HfOAuth2\Controller;
 
 use Hyperf\HttpServer\Annotation\Controller;
+use Hyperf\HttpServer\Annotation\GetMapping;
+use Hyperf\HttpServer\Annotation\Middleware;
+use Hyperf\HttpServer\Annotation\PostMapping;
+use Hyperf\RateLimit\Annotation\RateLimit;
 use Hyperf\Stringable\Str;
+use Jot\HfOAuth2\Annotation\Scope;
 use Jot\HfOAuth2\Entity\ClientEntity;
+use Jot\HfOAuth2\Middleware\CheckCredentials;
 use Jot\HfOAuth2\Repository\ClientRepository;
+use Jot\HfRepository\EntityInterface;
 use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 use function Hyperf\Support\make;
 
-#[Controller]
+#[Controller(prefix: '/oauth')]
 class ClientController extends AbstractController
 {
 
     protected string $repository = ClientRepository::class;
 
-    /**
-     * Creates a new client, validates the client data, and saves the client if validation passes.
-     *
-     * @return PsrResponseInterface Returns a response with a status code of 400 and validation errors if the client data is invalid,
-     *                               or a response with the saved client details if the process is successful.
-     *
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
-     */
+    #[PostMapping(path: '/clients')]
+    #[RateLimit(create: 1, capacity: 2)]
+    #[Scope(allow: 'oauth:client:create')]
+    #[Middleware(CheckCredentials::class)]
     public function createClient(): PsrResponseInterface
     {
         [$plainSecret, $client] = $this->createNewClient();
@@ -36,14 +38,17 @@ class ClientController extends AbstractController
         return $this->saveClient($client, $plainSecret);
     }
 
-    /**
-     * Generates a new client secret, creates a client entity with the provided data, and returns the secret and client entity.
-     *
-     * @return array Returns an array consisting of the newly generated plain secret and the client entity.
-     *
-     * @throws \Psr\Container\ContainerExceptionInterface
-     * @throws \Psr\Container\NotFoundExceptionInterface
-     */
+    #[GetMapping(path: 'clients')]
+    #[RateLimit(create: 1, capacity: 2)]
+    #[Scope(allow: 'oauth:client:list')]
+    #[Middleware(CheckCredentials::class)]
+    public function listClients(): PsrResponseInterface
+    {
+        $repository = $this->container->get(ClientRepository::class);
+        $clients = $repository->paginate([]);
+        return $this->response->json($clients);
+    }
+
     private function createNewClient(): array
     {
         $repository = $this->container->get(ClientRepository::class);
@@ -60,16 +65,7 @@ class ClientController extends AbstractController
         return [$plainSecret, $client];
     }
 
-    /**
-     * Saves a new client record and returns the client data along with its secret.
-     *
-     * @param ClientEntity $client The client entity to be saved.
-     * @param string $plainSecret The plain text secret associated with the client.
-     *
-     * @return PsrResponseInterface The response containing the created client data and secret,
-     *                              or an error message if an exception occurs.
-     */
-    private function saveClient(ClientEntity $client, string $plainSecret): PsrResponseInterface
+    private function saveClient(EntityInterface $client, string $plainSecret): PsrResponseInterface
     {
         $repository = $this->container->get(ClientRepository::class);
 
@@ -86,12 +82,6 @@ class ClientController extends AbstractController
         }
     }
 
-    /**
-     * Encrypts the provided secret using a secure HMAC-SHA256 algorithm.
-     *
-     * @param string $secret The secret string to be encrypted.
-     * @return string The resulting HMAC-SHA256 encrypted string.
-     */
     private function encryptSecret(string $secret): string
     {
         return hash_hmac('sha256', $secret, $this->config['encryption_key']);
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,13 +4,14 @@ namespace Jot\HfOAuth2\Controller;
 
 use Hyperf\HttpServer\Annotation\Controller;
 use Hyperf\HttpServer\Annotation\DeleteMapping;
+use Hyperf\HttpServer\Annotation\Middleware;
 use Hyperf\HttpServer\Annotation\PostMapping;
 use Hyperf\HttpServer\Annotation\PutMapping;
 use Hyperf\RateLimit\Annotation\RateLimit;
-use Hyperf\Stringable\Str;
-use Jot\HfOAuth2\Entity\UserEntity;
+use Jot\HfOAuth2\Annotation\Scope;
+use Jot\HfOAuth2\Entity\User\User;
+use Jot\HfOAuth2\Middleware\CheckCredentials;
 use Jot\HfOAuth2\Repository\UserRepository;
-use Jot\HfRepository\Exception\EntityValidationWithErrorsException;
 use Psr\Http\Message\ResponseInterface as PsrResponseInterface;
 use function Hyperf\Support\make;
 
@@ -20,54 +21,45 @@ class UserController extends AbstractController
 
     protected string $repository = UserRepository::class;
 
-    /**
-     * Creates a new user by processing provided user data, generating a password salt,
-     * and validating the user entity. If validation fails, it returns a response with errors.
-     *
-     * @return PsrResponseInterface Returns a response containing the saved user data or validation errors.
-     */
     #[PostMapping(path: 'users')]
+    #[Scope(allow: 'oauth:user:create')]
     #[RateLimit(create: 1, capacity: 2)]
+    #[Middleware(CheckCredentials::class)]
     public function createUser(): PsrResponseInterface
     {
         $userData = $this->request->all();
-        $userData['password_salt'] = Str::uuid()->toString();;
-        $user = make(UserEntity::class, ['data' => $userData]);
+        $user = make(User::class, ['data' => $userData]);
 
         return $this->saveUser($user);
     }
 
-    #[PutMapping(path: 'users/{id}')]
+    #[DeleteMapping(path: 'users/{id}')]
     #[RateLimit(create: 1, capacity: 2)]
+    #[Scope(allow: 'oauth:user:delete')]
+    private function saveUser(User $user): PsrResponseInterface
+    {
+        $createdUser = $this->repository()->create($user);
+        $userData = $createdUser
+            ->hide(['password_salt', 'password'])
+            ->toArray();
+        return $this->response->json($userData);
+    }
+
+    #[PutMapping(path: 'users/{id}')]
+    #[Scope(allow: 'oauth:user:update')]
+    #[RateLimit(create: 1, capacity: 2)]
+    #[Middleware(CheckCredentials::class)]
     public function updateUser(string $id): PsrResponseInterface
     {
         $userData = $this->request->all();
         $userData['id'] = $id;
-        $user = make(UserEntity::class, ['data' => $userData]);
+        $user = make(User::class, ['data' => $userData]);
         $user->setEntityState('update');
         if (!$user->validate()) {
             return $this->response->withStatus(400)->json($user->getErrors());
         }
         $updatedUser = $this->repository()->update($user);
         return $this->response->json($updatedUser->toArray());
-    }
-
-    /**
-     * Saves a user entity to the repository and returns a JSON response.
-     *
-     * @param UserEntity $user The user entity to be saved.
-     * @return PsrResponseInterface Returns a JSON response containing the created user data or an error message on failure.
-     * @throws EntityValidationWithErrorsException
-     */
-    #[DeleteMapping(path: 'users/{id}')]
-    #[RateLimit(create: 1, capacity: 2)]
-    private function saveUser(UserEntity $user): PsrResponseInterface
-    {
-        $createdUser = $this->repository()->createUser($user);
-        $userData = $createdUser
-            ->hide(['password_salt', 'password'])
-            ->toArray();
-        return $this->response->json($userData);
     }
 
 }
