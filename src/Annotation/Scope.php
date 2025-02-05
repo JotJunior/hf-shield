a82d57f36c31261900867d285ffcb7ded886d1c5
Joao G Zanon Jr <Joao Zanon>
2025-02-05 03:14:24 -0300

    Creating the Scope annotation

---
+++ src/Annotation/Scope.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jot\HfOAuth2\Annotation;
+
+use Attribute;
+use Hyperf\Di\Annotation\AbstractAnnotation;
+
+#[Attribute(Attribute::TARGET_CLASS | Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE)]
+class Scope extends AbstractAnnotation
+{
+
+    public function __construct(
+        public string|array $allow = []
+    )
+    {
+    }
+
+
+}
