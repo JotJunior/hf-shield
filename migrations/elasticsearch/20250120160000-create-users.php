d2fa4ae46ad475b69d91e98292e38b49a4aa5034
Joao Zanon <jot@jot.com.br>
2025-05-18 15:10:22 -0300

    fix: users migration

--- migrations/elasticsearch/20250120160000-create-users.php
+++ migrations/elasticsearch/20250120160000-create-users.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 /**
- * This file is part of the hf_shield module, a package build for Hyperf framework that is responsible for OAuth2 authentication and access control.
+ * This file is part of Gekom APIv2.
  *
- * @author   Joao Zanon <jot@jot.com.br>
- * @link     https://github.com/JotJunior/hf-shield
- * @license  MIT
+ * @document https://github.com/JotJunior/gekom
+ * @author   Joao Zanon <jot@jot.con.br>
+ * @link     https://gekom.com.br
+ * @license  Private
  */
 use Hyperf\Context\ApplicationContext;
 use Jot\HfElastic\Migration;
@@ -23,41 +24,45 @@ return new class(ApplicationContext::getContainer()) extends Migration {
 
         // basic user data
         $index->addField('keyword', 'id');
+        $index->addField('keyword', 'legacy_id');
         $index->addField('keyword', 'name')->normalizer('normalizer_ascii_lower')->searchable();
-        $index->addField('keyword', 'email');
-        $index->addField('keyword', 'phone');
-        $index->addField('keyword', 'federal_document');
+        $index->addField('keyword', 'language');
+        $index->addField('keyword', 'email')->searchable();
+        $index->addField('keyword', 'phone')->searchable();
+        $index->addField('keyword', 'federal_document')->searchable();
         $index->addField('keyword', 'document_type');
         $index->addField('keyword', 'picture');
         $index->addField('keyword', 'password_salt');
         $index->addField('keyword', 'password');
-        $index->addField('keyword', 'tags');
         $index->addField('keyword', 'status');
-        $index->addField('text', 'custom_settings');
+        $index->addField('keyword', 'tags');
+        $index->addField('dynamic_object', 'custom_settings');
 
         $tenant = new Migration\ElasticType\ObjectType('tenant');
         $tenant->addField('keyword', 'id');
         $tenant->addField('keyword', 'name')->normalizer('normalizer_ascii_lower');
         $index->object($tenant);
 
-        $tenant = new Migration\ElasticType\NestedType('tenants');
-        $tenant->addField('keyword', 'id');
-        $tenant->addField('keyword', 'name');
-
+        $tenants = new Migration\ElasticType\NestedType('tenants');
+        $tenants->addField('keyword', 'id');
+        $tenants->addField('keyword', 'name')->normalizer('normalizer_ascii_lower');
         $scopes = new Migration\ElasticType\NestedType('scopes');
         $scopes->addField('keyword', 'id');
         $scopes->addField('keyword', 'name')->normalizer('normalizer_ascii_lower');
         $scopes->addField('keyword', 'domain');
         $scopes->addField('keyword', 'resource');
         $scopes->addField('keyword', 'action');
-        $tenant->nested($scopes);
-
+        $tenants->nested($scopes);
         $groups = new Migration\ElasticType\NestedType('groups');
         $groups->addField('keyword', 'id');
         $groups->addField('keyword', 'name')->normalizer('normalizer_ascii_lower');
-        $tenant->nested($groups);
-
-        $index->nested($tenant);
+        $tenants->nested($groups);
+        $customers = new Migration\ElasticType\NestedType('customers');
+        $customers->addField('keyword', 'id');
+        $customers->addField('keyword', 'trade_name')->normalizer('normalizer_ascii_lower');
+        $customers->addField('keyword', 'legal_name')->normalizer('normalizer_ascii_lower');
+        $tenants->nested($customers);
+        $index->nested($tenants);
 
         $index->alias('user_id')->path('id');
         $index->defaults();
