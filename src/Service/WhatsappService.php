4b72360852eb1734b046d641722642d3e76f00c0
Joao Zanon <jot@jot.com.br>
2025-05-20 10:37:38 -0300

    feat: Twilio sender

---
+++ src/Service/WhatsappService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Jot\HfShield\Service;
+
+use Hyperf\Contract\ConfigInterface;
+use Psr\Log\LoggerAwareTrait;
+use Twilio\Exceptions\TwilioException;
+use Twilio\Rest\Client;
+
+class WhatsappService
+{
+
+    use LoggerAwareTrait;
+
+    protected Client $sender;
+
+    private array $credentials;
+
+    public function __construct(
+        private readonly ConfigInterface $config
+    )
+    {
+        $credentials = $this->config->get('twilio.credentials');
+        $this->credentials = $credentials;
+        $this->sender = new Client($credentials['sid'], $credentials['token']);
+    }
+
+    public function send(string $to, array $parameters): void
+    {
+        try {
+            $result = $this->sender->messages->create(
+                to: $to,
+                options: [
+                    'from' => empty($parameters['from']) ? $this->credentials['from'] : $parameters['from'],
+                    'contentVariables' => json_encode($parameters['variables']),
+                    'contentSid' => $parameters['content_sid'],
+                    'messagingServiceSid' => empty($parameters['messaging_service']) ? $this->credentials['messaging_service'] : $parameters['messaging_service'],
+                ]
+            );
+            $this->logger->info(sprintf('OTP sent to %s', $to), $result->toArray());
+        } catch (TwilioException $th) {
+            $this->logger->error($th->getMessage());
+        }
+    }
+
+}
