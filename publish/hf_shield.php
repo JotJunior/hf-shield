015cbe271f0c7cbbd152a51a8e8d0c01e34e13ab
Joao Zanon <jot@jot.com.br>
2025-05-24 14:15:58 -0300

    feat: otp amqp producer

--- publish/hf_shield.php
+++ publish/hf_shield.php
@@ -1,7 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
 /**
  * This file is part of the hf_shield module, a package build for Hyperf framework that is responsible for OAuth2 authentication and access control.
  *
@@ -9,11 +8,9 @@ declare(strict_types=1);
  * @link     https://github.com/JotJunior/hf-shield
  * @license  MIT
  */
-
 use function Hyperf\Support\env;
 
 return [
-
     /*
      * The auth options available for the user.
      * The default value is ['bearer', 'session', 'webauthn', 'whatsapp'].
---
+++ src/Amqp/Producer/Otp.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of the hf_shield module, a package build for Hyperf framework that is responsible for OAuth2 authentication and access control.
+ *
+ * @author   Joao Zanon <jot@jot.com.br>
+ * @link     https://github.com/JotJunior/hf-shield
+ * @license  MIT
+ */
+
+namespace Jot\HfShield\Amqp\Producer;
+
+use Hyperf\Amqp\Annotation\Producer;
+use Hyperf\Amqp\Message\ProducerMessage;
+
+#[Producer(exchange: 'hf-shield', routingKey: 'otp.sender')]
+class Otp extends ProducerMessage
+{
+    public function __construct($data)
+    {
+        $this->payload = $data;
+    }
+}
